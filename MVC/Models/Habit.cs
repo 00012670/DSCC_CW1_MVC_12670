5d89270cbeed63f811f421c0fb2f187bec3ed683
00012670 <wiut00012670@gmail.com>
2023-11-04 21:04:43 +0500

    fix validation for habits, modify create and edit views

--- MVC/Models/Habit.cs
+++ MVC/Models/Habit.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations; 
 
 namespace MVC.Models
 {
     public class Habit
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Frequency is required")]
         public int Frequency { get; set; }
+
         public TargerFrequency Repeat { get; set; }
+
+        [Required(ErrorMessage = "Start Date is required")]
         public DateTime StartDate { get; set; }
 
         public enum TargerFrequency
